831706fad5f550e18e7a0e74ce299af41d6991e1
Phillip Burleigh <phillip_burleigh@redmane.com>
2024-08-13 09:23:34 -0500

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "./App.css";
 import Header from "./components/Header";
 import Home from "./views/Home";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="cardContainer">
+    <div>
       <Header />
-      <Home />
+      <div className="px-md-5 d-inline-block">
+        <Home />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row } from "react-bootstrap";
+
+const Footer = () => {
+  return (
+    <footer className="pdb-footer footer py-3 bg-body-tertiary">
+      <Container fluid>
+        <Row>
+          <h2>Footer</h2>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
