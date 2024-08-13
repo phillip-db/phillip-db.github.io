4ddc22e929407468a7cd9ec7ee2392a24842d4a7
Phillip Burleigh <phillip_burleigh@redmane.com>
2024-08-12 21:13:17 -0500

    Implement theme switching

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,16 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import ThemeIcon from "./ThemeIcon";
 
 const Header = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary rounded-2">
       <Container>
-        <Navbar.Brand href="/">phillip.db</Navbar.Brand>
+        <Navbar.Brand href="#Home">phillip.db</Navbar.Brand>
+        <ThemeIcon />
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#Home">Home</Nav.Link>
             <Nav.Link href="#About">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
