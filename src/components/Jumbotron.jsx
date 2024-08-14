3769113d2a11cf4dc59241d78020ecb4ba429edf
Phillip Burleigh <phillip_burleigh@redmane.com>
2024-08-14 15:53:29 -0500

    Homepage changes

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -5,30 +5,38 @@ const Jumbotron = () => {
   return (
     <Container className="jumbotron">
       <div className="p-5 text-center bg-body-tertiary rounded-3">
-        {/* <i className="bi bi-terminal-fill i-jumbo"></i> */}
-        <div className="pb-3 mx-auto" style={{ width: "10rem" }}>
+        <div
+          className="pb-3 mx-auto text-body-emphasis"
+          style={{ width: "10rem" }}
+        >
           <Logo />
         </div>
         <h1 className="text-body-emphasis">Hello there!</h1>
         <p className="col-lg-8 mx-auto fs-5 text-muted">
-          This is a custom jumbotron featuring an SVG image at the top, some
-          longer text that wraps early thanks to a responsive{" "}
-          <code>.col-*</code> className, and a customized call to action.
+          Welcome to my developer portfolio built with{" "}
+          <code>
+            <a className="code-link" href="https://react.dev/" target="_blank">
+              React
+            </a>
+          </code>
+          ,{" "}
+          <code>
+            <a className="code-link" href="https://vitejs.dev/" target="_blank">
+              Vite
+            </a>
+          </code>
+          , and{" "}
+          <code>
+            <a
+              className="code-link"
+              href="https://react-bootstrap.netlify.app/"
+              target="_blank"
+            >
+              React Bootstrap
+            </a>
+          </code>
+          .
         </p>
-        <div className="d-inline-flex gap-2 mb-5">
-          <button
-            className="d-inline-flex align-items-center btn btn-primary btn-lg px-4 rounded-pill"
-            type="button"
-          >
-            Call to action
-          </button>
-          <button
-            className="btn btn-outline-secondary btn-lg px-4 rounded-pill"
-            type="button"
-          >
-            Secondary link
-          </button>
-        </div>
       </div>
     </Container>
   );
